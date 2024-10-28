221a24ff6ff14b058a200031ee798f4411399535
harith-hacky03 <harithhacky03@gmail.com>
2024-10-29 00:54:00 +0530

    feat: add hover animation and reveal details functionality

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,7 +1,9 @@
+import React from 'react';
+
 const Card = ({ user }) => {
   return (
-    <div className="shadow-md bg-white rounded-lg w-80 h-60 flex flex-col justify-between p-4"> {/* Fixed width and height */}
-      <div className="text-gray-500 text-sm overflow-hidden text-ellipsis whitespace-nowrap">
+    <div className="relative transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 shadow-md bg-white rounded-lg w-80 h-60 flex flex-col justify-between p-4 group">
+      <div className="text-gray-500 flex justify-between text-sm overflow-hidden text-ellipsis whitespace-nowrap">
         <span className='cursor-pointer'>{user.website}</span>
       </div>
       <hr className="my-2" />
@@ -34,6 +36,11 @@ const Card = ({ user }) => {
           src='https://img.icons8.com/?size=100&id=85353&format=png&color=000000' 
         />
         <div className='text-sm px-2'>{user.address.city}</div>
+      </div>
+      <div className="absolute inset-0 flex left-[60%] top-2 w-[80%] h-8 md:opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+        <button className="bg-blue-600 text-sm text-white rounded-lg px-4  transform transition-all duration-300">
+          View Details
+        </button>
       </div>
     </div>
   );
