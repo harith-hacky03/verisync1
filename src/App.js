110547d01f0737612fea5f5290458b0081e4cb8a
harith-hacky03 <harithhacky03@gmail.com>
2024-10-28 23:30:40 +0530

    chore: remove navbar component from App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
-import React from 'react'
-import Navbar from './Components/Navbar'
+
 import Hero from './Components/Hero'
 const App = () => {
   return (
-    <div className='min-h-screen bg-gray-50'>
-        <div>
-            <Navbar/>
-        </div>
+    <div className='min-h-screen bg-gray-100'>
         <div>
             <Hero/>
         </div>
