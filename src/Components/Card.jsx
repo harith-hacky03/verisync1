44a5a8cd273b6737d3eeafe3aedb952de8601a3c
harith-hacky03 <harithhacky03@gmail.com>
2024-10-29 00:55:28 +0530

    style: reduce scale effect on hover

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Card = ({ user }) => {
   return (
-    <div className="relative transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 shadow-md bg-white rounded-lg w-80 h-60 flex flex-col justify-between p-4 group">
+    <div className="relative transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-105 duration-300 shadow-md bg-white rounded-lg w-80 h-60 flex flex-col justify-between p-4 group">
       <div className="text-gray-500 flex justify-between text-sm overflow-hidden text-ellipsis whitespace-nowrap">
         <span className='cursor-pointer'>{user.website}</span>
       </div>
