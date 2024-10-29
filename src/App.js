945930c2036a61dcaf673dcfd99f28a7dc28c269
harith-hacky03 <harithhacky03@gmail.com>
2024-10-29 15:34:15 +0530

    feat: implement React Router DOM for navigation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import React from 'react'
-import RegistrationForm from './Components/Registration'
-const App = () => {
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import LoginForm from './Components/Login';
+import RegistrationForm from './Components/Registration';
+
+function App() {
   return (
-    <div>
-      <RegistrationForm/>
-    </div>
-  )
+    <Router>
+      <Routes>
+        <Route path="/login" element={<LoginForm />} />
+        <Route path="/" element={<RegistrationForm />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
